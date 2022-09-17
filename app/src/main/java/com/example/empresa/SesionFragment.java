b8a290142ad395e11cc9efe1ee5f91bdbf6a6365
505 <505@academicos.cesde.edu.co>
2022-09-16 19:27:57 -0500

    16-09-22

--- app/src/main/java/com/example/empresa/SesionFragment.java
+++ app/src/main/java/com/example/empresa/SesionFragment.java
@@ -1,5 +1,6 @@
 package com.example.empresa;
 
+import android.content.ContentValues;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
@@ -35,6 +39,7 @@ public class SesionFragment extends Fragment implements Response.Listener<JSONOb
         // Inflate the layout for this fragment
         //return inflater.inflate(R.layout.fragment_sesion, container, false);
         View vista = inflater.inflate(R.layout.fragment_sesion,container,false);
+        rq= Volley.newRequestQueue(getContext());//requerimiento volley
         jetCorreo=vista.findViewById(R.id.etcorreo);
         jetClave=vista.findViewById(R.id.etclave);
         jbtingresar=vista.findViewById(R.id.btingresar);
@@ -42,19 +47,39 @@ public class SesionFragment extends Fragment implements Response.Listener<JSONOb
         jbtingresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Iniciar_Sesion();
             }
         });
+
+
                 return vista;
+
+
+
     }
+ private void Iniciar_Sesion(){
+        String correo,clave;
+        correo=jetCorreo.getText().toString();
+        clave=jetClave.getText().toString();
+        if (correo.isEmpty()|| clave.isEmpty())
+        {
+            Toast.makeText(getContext() ,"correo y clave son requeridos",Toast.LENGTH_LONG).show();
+        }else
+        {
+            String url = "http://172.16.60.31:8080/WebServices/sesion.php?correo="+correo+"&clave="+clave;
+            jrq = new JsonObjectRequest(Request.Method.GET,url,null,this,this);
+            rq.add(jrq);
+        }
+ }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+     Toast.makeText(getContext(),"Error en inicio de sesion",Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onResponse(JSONObject response) {
+        Toast.makeText(getContext(),"Sesion iniciada",Toast.LENGTH_LONG).show();
 
     }
 }
